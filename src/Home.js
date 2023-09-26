819f905a32d5f14d3a6c82fabce01332d4a1d30d
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-09-26 15:58:04 +0530

    Build a navbar component with navbar like functionality.

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const Home = ()=>{
     return (
         <div>
+            <Navbar/>
             <h1>Home Page</h1>
             <br></br>
             <p>This is our Home Page for this Web Application.</p>
